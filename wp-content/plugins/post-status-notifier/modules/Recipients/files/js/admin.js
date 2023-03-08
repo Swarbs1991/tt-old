df63d47f3dbfba6cad2473c970d9a547f1295394
Swarbs <97552857+Swarbs1991@users.noreply.github.com>
2023-03-08 13:58:26 +0000

    Deleeted heatmap plugin

--- wp-content/plugins/post-status-notifier/modules/Recipients/files/js/admin.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready( function($) {
-
-    $('a.import_items_container_toggle').on('click', function(event) {
-        event.stopPropagation();
-        $('#import_items_container').fadeToggle( "slow", "linear" );
-        return false;
-    });
-
-});
